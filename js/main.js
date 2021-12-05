8dcc2ab54550b19276e113a555cbca38d3070270
Pedro-relax <77356797+Pedro-relax@users.noreply.github.com>
2021-12-05 16:48:20 -0600

    Javascript done

--- js/main.js
+++ js/main.js
@@ -12,6 +12,8 @@ document.addEventListener('DOMContentLoaded',function(){
 	console.log ("It loaded");
 	document.getElementById('submit').addEventListener('click', function () {
 		var full = document.getElementById('fullname').value;
+		var  ma = document.getElementById('mail').value;
+		var ph =document.getElementById('phone').value;
 	
 		if ( full === "") {
 			alert("Enter Full Name Please");
@@ -19,8 +21,18 @@ document.addEventListener('DOMContentLoaded',function(){
 		else {
 			alert ( "Hello " + full + " You are now a member");
 		}
+		if (ma === "") {
+			alert( "Enter your email please");
+		else {
+			alert (" A Email was sent to " + ma);
+		}
+		if (ph === "") {
+			alert("Enter your phone number please");
+		}
+		else {
+			alert ("Thank you for entering your phone number, you will receive a notification shortly");
+		}
 	});
-		document.getElementById('mail').disabled=true;
-		document.getElementById('phone').disabled=true;
+		
 		
 	});
